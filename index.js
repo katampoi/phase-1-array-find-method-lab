33e24ad4bf29a1091605221248ff611f4432f0fa
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-11 14:00:50 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // code your solution here
-const superbowlWin = (array) => {
-    for(const item of array){
-    if(item.result === "W"){
-    return item.year;
+// const superbowlWin = (array) => {
+//     for(const item of array){
+//     if(item.result === "W"){
+//     return item.year;
+// }
+// }
+// }
+let array = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+];
+const superbowlWin = (array) =>{
+    let result = array.find((item) => item.result === 'W')
+    if (result) {
+        return result.year
+    } else{
+        return undefined
 }
 }
-}
+
     
 
 
